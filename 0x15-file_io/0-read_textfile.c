a051c59f401d29e9033de24c27845ac42ddb4763
Hugo Cortes <hugocortesmu@gmail.com>
2021-03-30 11:07:11 -0500

    file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,43 +1,45 @@
+#include "holberton.h"
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <sys/uio.h>
-#include <unistd.h>
 #include <fcntl.h>
 #include <stdlib.h>
+
 /**
-* read_textfile - reads a text file and prints the contents to STDOUT
-* @filename: the filename to open
-* @letters: number of letters to read and print
-*
-* Return: the number of letters read and printed, 0 on failure
-*/
+  * read_textfile - read a text file and read it
+  * prints to standard POSIX output
+  * @filename: name of the file to read
+  * @letters: number of letters to read and print
+  *
+  * Return: actual number of letters you can read and print
+  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-int fd = -1, err = 0, len = 0;
-char *temp_b;
-if (!filename)
-return (0);
-temp_b = malloc(sizeof(char) * letters);
-if (!temp_b)
-return (0);
-fd = open(filename, O_RDONLY);
-if (fd < 0)
-{
-free(temp_b);
-return (0);
-}
-err = read(fd, temp_b, letters);
-if (err < 0)
-{
-free(temp_b);
-return (0);
-}
-while (temp_b && temp_b[len])
-len++;
-err = write(STDOUT_FILENO, temp_b, len);
-free(temp_b);
-close(fd);
-if (err < 0)
-return (0);
-return ((ssize_t)err);
+	int fed;
+	ssize_t lennr, lennw;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+	fed = open(filename, O_RDONLY);
+	if (fed == -1)
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		close(fed);
+		return (0);
+	}
+	lennr = read(fed, buffer, letters);
+	close(fed);
+	if (lennr == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+	lennw = write(STDOUT_FILENO, buffer, lennr);
+	free(buffer);
+	if (lennr != lennw)
+		return (0);
+	return (lennw);
 }
