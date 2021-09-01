902d0c64ca00884665a33f8132d73e62b925b983
Hugo Cortes <hugocortesmu@gmail.com>
2021-08-31 23:54:54 -0500

    library

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,7 +5,7 @@
 
 int linear_search(int *array, size_t size, int value);
 void print_message(int *array, size_t size);
-size_t get_middle(size_t start, size_t end);
 int binary_search(int *array, size_t size, int value);
+size_t get_middle(size_t start, size_t end);
 
 #endif
